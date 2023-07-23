108807a87d9cef310642775f292f949fee4fe98d
pinchiyadav <pinchiyadav@gmail.com>
2023-07-24 01:18:45 +0530

    sms viewed with time

--- app/src/main/java/com/example/smsapp/SMSActivity.kt
+++ app/src/main/java/com/example/smsapp/SMSActivity.kt
@@ -6,8 +6,13 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Telephony
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -20,6 +25,7 @@ class SMSActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sms)
 
+        val contactName = intent.getStringExtra("CONTACT_NAME")
         val contactNumber = intent.getStringExtra("CONTACT_NUMBER")
 
         if (ContextCompat.checkSelfPermission(
@@ -33,29 +39,33 @@ class SMSActivity : AppCompatActivity() {
                 requestSmsPermission
             )
         } else {
-            contactNumber?.let { displaySMS(it) }
+            contactName?.let {
+                contactNumber?.let { number ->
+                    displaySMS(it, number)
+                }
+            }
         }
     }
 
-    private fun displaySMS(contactNumber: String) {
+    private fun displaySMS(contactName: String, contactNumber: String) {
         val smsList = getSMSList(contactNumber)
         if (smsList.isEmpty()) {
-            Toast.makeText(this, "No SMS found with $contactNumber.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "No SMS found with $contactName.", Toast.LENGTH_SHORT).show()
         } else {
             val adapter =
-                ArrayAdapter(this, android.R.layout.simple_list_item_1, smsList)
+                SMSAdapter(this, smsList)
             val smsListView: ListView = findViewById(R.id.smsListView)
             smsListView.adapter = adapter
         }
     }
 
-    private fun getSMSList(contactNumber: String): List<String> {
-        val smsList = mutableListOf<String>()
-        val projection = arrayOf(Telephony.Sms.ADDRESS, Telephony.Sms.BODY, Telephony.Sms.TYPE)
+    private fun getSMSList(contactNumber: String): List<SMSItem> {
+        val smsList = mutableListOf<SMSItem>()
+        val projection = arrayOf(Telephony.Sms.ADDRESS, Telephony.Sms.BODY, Telephony.Sms.TYPE, Telephony.Sms.DATE)
 
         val uri: Uri = Telephony.Sms.CONTENT_URI
-        val selection = "address = ?"
-        val selectionArgs = arrayOf(contactNumber)
+        val selection = "address = ? OR address = ?"
+        val selectionArgs = arrayOf(contactNumber, contactNumber)
         val cursor: Cursor? = contentResolver.query(
             uri,
             projection,
@@ -68,14 +78,16 @@ class SMSActivity : AppCompatActivity() {
             val addressIndex = it.getColumnIndex(Telephony.Sms.ADDRESS)
             val bodyIndex = it.getColumnIndex(Telephony.Sms.BODY)
             val typeIndex = it.getColumnIndex(Telephony.Sms.TYPE)
+            val dateIndex = it.getColumnIndex(Telephony.Sms.DATE)
 
             while (it.moveToNext()) {
                 val address = it.getString(addressIndex)
                 val body = it.getString(bodyIndex)
                 val type = it.getInt(typeIndex)
+                val date = it.getLong(dateIndex)
 
                 val smsDirection = if (type == Telephony.Sms.MESSAGE_TYPE_INBOX) "Received" else "Sent"
-                val smsDetails = "$smsDirection: $address\n$body"
+                val smsDetails = SMSItem("$smsDirection: $address", body, date)
                 smsList.add(smsDetails)
             }
         }
@@ -92,8 +104,13 @@ class SMSActivity : AppCompatActivity() {
         when (requestCode) {
             requestSmsPermission -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    val contactName = intent.getStringExtra("CONTACT_NAME")
                     val contactNumber = intent.getStringExtra("CONTACT_NUMBER")
-                    contactNumber?.let { displaySMS(it) }
+                    contactName?.let {
+                        contactNumber?.let { number ->
+                            displaySMS(it, number)
+                        }
+                    }
                 } else {
                     Toast.makeText(this, "SMS permission denied.", Toast.LENGTH_SHORT).show()
                 }
@@ -102,5 +119,36 @@ class SMSActivity : AppCompatActivity() {
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults)
             }
         }
+    }
+}
+
+data class SMSItem(val address: String, val body: String, val date: Long)
+
+class SMSAdapter(
+    context: AppCompatActivity,
+    private val smsList: List<SMSItem>
+) : ArrayAdapter<SMSItem>(context, 0, smsList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_sms, parent, false)
+        }
+
+        val smsItem = smsList[position]
+        val address = smsItem.address
+        val body = smsItem.body
+        val date = smsItem.date
+
+        val addressTextView: TextView = view!!.findViewById(R.id.addressTextView)
+        addressTextView.text = address
+
+        val bodyTextView: TextView = view.findViewById(R.id.bodyTextView)
+        bodyTextView.text = body
+
+        val dateTextView: TextView = view.findViewById(R.id.dateTextView)
+        dateTextView.text = DateUtils.formatDateTime(context, date, DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_YEAR)
+
+        return view
     }
 }

133627e19b868e1ebd4e0055d990f1d9f99e12e4
pinchiyadav <pinchiyadav@gmail.com>
2023-07-24 01:02:55 +0530

    sms viewed

--- app/src/main/java/com/example/smsapp/ContactsActivity.kt
+++ app/src/main/java/com/example/smsapp/ContactsActivity.kt
@@ -1,14 +1,20 @@
 package com.example.smsapp
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.provider.Telephony
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -53,19 +59,20 @@ class ContactsActivity : AppCompatActivity() {
             if (contactsList.isEmpty()) {
                 Toast.makeText(this, "No contacts with SMS found.", Toast.LENGTH_SHORT).show()
             } else {
-                val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactsList)
+                val adapter = ContactAdapter(this, contactsList)
                 val contactListView: ListView = findViewById(R.id.contactListView)
                 contactListView.adapter = adapter
                 contactListView.setOnItemClickListener { _, _, position, _ ->
-                    val selectedContactNumber = contactsList[position]
-                    openSMSActivity(selectedContactNumber)
+                    val selectedContactNumber = contactsList[position].second
+                    val selectedContactName = contactsList[position].first
+                    openSMSActivity(selectedContactName, selectedContactNumber)
                 }
             }
         }
     }
 
-    private fun getContactsList(): List<String> {
-        val contactsList = mutableListOf<String>()
+    private fun getContactsList(): List<Pair<String, String>> {
+        val contactsList = mutableListOf<Pair<String, String>>()
         val projection = arrayOf(Telephony.Sms.ADDRESS)
 
         val uri: Uri = Telephony.Sms.CONTENT_URI
@@ -81,8 +88,9 @@ class ContactsActivity : AppCompatActivity() {
             val addressIndex = it.getColumnIndex(Telephony.Sms.ADDRESS)
             while (it.moveToNext()) {
                 val contactNumber = it.getString(addressIndex)
-                if (!contactsList.contains(contactNumber)) {
-                    contactsList.add(contactNumber)
+                val contactName = getContactNameFromNumber(contactNumber)
+                if (!contactsList.any { pair -> pair.second == contactNumber }) {
+                    contactsList.add(Pair(contactName, contactNumber))
                 }
             }
         }
@@ -91,8 +99,29 @@ class ContactsActivity : AppCompatActivity() {
         return contactsList
     }
 
-    private fun openSMSActivity(contactNumber: String) {
+    private fun getContactNameFromNumber(contactNumber: String): String {
+        val uri = Uri.withAppendedPath(
+            ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+            Uri.encode(contactNumber)
+        )
+        val projection = arrayOf(ContactsContract.PhoneLookup.DISPLAY_NAME)
+        var contactName = ""
+
+        val cursor: Cursor? = contentResolver.query(uri, projection, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                contactName =
+                    it.getString(it.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME))
+            }
+        }
+
+        cursor?.close()
+        return contactName
+    }
+
+    private fun openSMSActivity(contactName: String, contactNumber: String) {
         val intent = Intent(this, SMSActivity::class.java)
+        intent.putExtra("CONTACT_NAME", contactName)
         intent.putExtra("CONTACT_NUMBER", contactNumber)
         startActivity(intent)
     }
@@ -123,5 +152,28 @@ class ContactsActivity : AppCompatActivity() {
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults)
             }
         }
+    }
+}
+
+class ContactAdapter(
+    context: Context,
+    private val contactsList: List<Pair<String, String>>
+) : ArrayAdapter<Pair<String, String>>(context, 0, contactsList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
+        if (view == null) {
+            view = LayoutInflater.from(context)
+                .inflate(android.R.layout.simple_list_item_1, parent, false)
+        }
+
+        val contactItem = contactsList[position]
+        val contactName = contactItem.first
+        val contactNumber = contactItem.second
+
+        val textView: TextView = view as TextView
+        textView.text = "$contactName ($contactNumber)"
+
+        return view
     }
 }
